6ae1f1e9a5b46772995004bac84f0f980199d011
BugHunter-X <askzenai@gmail.com>
2024-12-16 17:14:08 +0100

    Add bugSolution.ts file

---
+++ bugSolution.ts
@@ -0,0 +1,15 @@
+function printNumbers(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers(5);
+
+function printNumbers1(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+printNumbers1(5); 
